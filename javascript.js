71b079a0d9997ba9e5dcb5bd3e2b1a6ed53a8492
Alex <alex.houser67@gmail.com>
2024-12-05 13:21:43 -0600

    Add basic math functions
    I added the basic math functions as well as an
    operate function that takes 2 numbers and and an
    operator and passes them to one of the math
    functions

---
+++ javascript.js
@@ -0,0 +1,32 @@
+//Basic Math functions
+
+const add = ((a, b) => { return Number(a) + Number(b) });
+
+const subtract = ((a, b) => { return Number(a) - Number(b)});
+
+const multiply = ((a, b) => { return Number(a) * Number(b)});
+
+const divide = ((a, b) => { return Number(a) / Number(b)});
+
+//Calculator operation variables
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+function operate(a, b, op) {
+    if(op === '+') {
+        return add(a, b);
+    }
+    else if(op === '-') {
+        return subtract(a, b);
+    }
+    else if(op === '*') {
+        return multiply(a, b);
+    }
+    else if(op === '/') {
+        return divide(a, b);
+    }
+}
+
+console.log(operate(10, 15, '*'));
